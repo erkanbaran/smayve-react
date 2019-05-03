acca354b6743bca3f6097fb5d2cd4f3d59a504b7
Erkan Baran <erkanbaran15@msn.com>
2019-05-03 12:21:40 +0100

    firstcommit

---
+++ src/components/header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const user={
+    name:'Barry',
+    lastname:'Allen',
+    age:28
+}
+
+// const Header = ()=>{
+//     return( 
+//     <div>
+//         <div>{user.name}</div>        
+//         <div>{user.lastname}</div>        
+//         <div>{user.age}</div>        
+//     </div>
+//     )
+// }
+
+class Header extends React.Component{
+
+    inputChangeHandler(event){
+        console.log(event.target.value)
+    }
+
+    render(){
+        return(
+            <header>
+                <div className="logo">
+                    Free Life in London
+                </div>
+
+                <p>How to survive without money!</p>
+                    
+                <input type="text" onChange={this.inputChangeHandler}></input>
+            </header>
+        )
+    }
+}
+
+export default Header;
